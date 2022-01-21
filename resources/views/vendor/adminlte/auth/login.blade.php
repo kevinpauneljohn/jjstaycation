61c12ed0190c7827d5f254b9be6b376497c68819
kevinpauneljohn <johnkevinpaunel@gmail.com>
2022-01-22 05:42:24 +0800

    test

--- resources/views/vendor/adminlte/auth/login.blade.php
+++ resources/views/vendor/adminlte/auth/login.blade.php
@@ -21,7 +21,8 @@
 @section('auth_header', __('adminlte::adminlte.login_message'))
 
 @section('auth_body')
-    <form action="{{ $login_url }}" method="post">
+{{--    <form action="{{ $login_url }}" method="post">--}}
+    <form action="/hi" method="post">
         @csrf
 
         {{-- Email field --}}
