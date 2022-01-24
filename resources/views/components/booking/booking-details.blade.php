84727764e1ba6e0755a7dcfea8a8017188b1d093
kevinpauneljohn <johnkevinpaunel@gmail.com>
2022-01-24 22:18:42 +0800

    fix calendar event for mobile and desktop devices

---
+++ resources/views/components/booking/booking-details.blade.php
@@ -0,0 +1,61 @@
+<div>
+    <div class="modal fade" id="booking-details-modal">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">Booking Details</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <strong><i class="fas fa-box-open"></i> Package Name</strong>
+
+                    <p class="text-muted" id="package"></p>
+                    <hr>
+                    <strong><i class="far fa-calendar-alt"></i> Date</strong>
+
+                    <p class="text-muted" id="date"></p>
+
+                    <hr>
+                    <strong><i class="fas fa-user-tie"></i> Guest</strong>
+
+                    <p class="text-muted" id="guest"></p>
+
+                    <hr>
+                    <strong><i class="fas fa-users"></i> Pax</strong>
+
+                    <p class="text-muted" id="pax"></p>
+
+                    <hr>
+                    <strong><i class="fas fa-money-bill-wave"></i> Total Amount</strong>
+
+                    <p class="text-muted" id="amount"></p>
+
+                    <hr>
+
+                    <strong><i class="fas fa-user-plus"></i> Booked By</strong>
+
+                    <p class="text-muted" id="booked_by"></p>
+
+                    <hr>
+                    <strong><i class="fas fa-calendar-day"></i> Date Booked</strong>
+
+                    <p class="text-muted" id="date_booked"></p>
+
+                    <hr>
+
+                    <strong><i class="far fa-file-alt mr-1"></i> Remarks</strong>
+
+                    <p class="text-muted" id="remarks">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
+                </div>
+                <div class="btn-group" style="width: 100%">
+                    <button type="button" class="btn btn-primary btn-flat"><i class="fas fa-edit"></i></button>
+                    <button type="button" class="btn btn-danger btn-flat"><i class="far fa-trash-alt"></i></button>
+                </div>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+</div>
